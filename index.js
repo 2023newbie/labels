858dd5b9f8dd78e374e150e6b4201bc1be5d1d52
Vu Duc <ducwebdev@gmail.com>
2024-05-18 09:50:04 +0700

    complete

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
-const labels = []
+let labels = []
 
 const containBox = document.querySelector('.flex2')
-const inputEle = document.querySelector('.input')
-const btnEle = document.querySelector('.button')
-
-btnEle.onclick = function() {
-    if (inputEle.value.trim() !== '') {
-        labels.push(inputEle.value.trim())
-        renderContainer()
-    }
-}
 
 const renderContainer = () => {
     containBox.innerHTML = `
         ${labels.map(label => {
             return `
-                <div class="flex">
+                <div class="flex" id="${label.id}">
                     <div class="x-icon">
                         <div class="top-bot"></div>
                         <div class="bot-top"></div>
                     </div>
-                    <div class="label">${label}</div>
+                    <div class="label">${label.value}</div>
                 </div>
             `
         }).join('')}
     `
+    containBox.innerHTML += `<input type="text" class="input">`
+    document.querySelector('.input').focus()
+
+    const xIconEles = document.querySelectorAll('.x-icon')
+    xIconEles.forEach(xIcon => {
+        xIcon.onclick = function() {
+            const id = this.parentElement.id
+            labels = labels.filter(label => label.id !== id)
+            renderContainer()
+        }
+    })
 }
+
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        const inputEle = document.querySelector('.input')
+        const valueInput = inputEle.value.trim()
+        const isInputFocus = document.activeElement === inputEle
+        if (valueInput && isInputFocus) {
+            labels.push({
+                value: valueInput,
+                id: Math.random().toString()
+            })
+            renderContainer()
+        }
+    }
+})
 
 renderContainer()
